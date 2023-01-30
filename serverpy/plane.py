014f5582ae44b1cd09d4e70e173ab25453fef9a1
Burak Canbul <burakcanbul41@gmail.com>
2023-01-30 15:12:10 +0300

    req to req.id

--- serverpy/plane.py
+++ serverpy/plane.py
@@ -30,7 +30,7 @@ class PlaneServicer(planes_pb2_grpc.PlaneServicer):
         for item in planeList:
             yield item.toGetResp()
     def deletePlane(self, request:planes_pb2.delPlaneReq, context):
-        if len(planeList)>request.id or request<0:
+        if len(planeList)>request.id or request.id<0:
             return planes_pb2.delPlaneRep(repMessage="Couldn't Fount plane with index")
         if planeList[request.id]:
             planeList.pop(request.id)
